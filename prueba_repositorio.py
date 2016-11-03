4671a00115e60e429df8ad59d9c681047272b9e2
carber <carberlaf@gmail.com>
2016-11-03 10:40:24 +0100

    modificado el docstring con rst

--- prueba_repositorio.py
+++ prueba_repositorio.py
@@ -1,6 +1,12 @@
 """
-Archivo creado para subir al github de prueba
-_author_ = 'carber'
+===
+DOC
+===
+
+
+Archivo *creado* para subir al **github** de prueba
+:author: carber
+:VERSION: 1.000
 """
 
 print("Hola a todos")
